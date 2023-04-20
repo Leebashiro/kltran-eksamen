0030bb236f664d83e65343d3ebc023f1d3b094bb
Leebashiro <111729362+Leebashiro@users.noreply.github.com>
2023-04-20 22:24:24 +0200

    Link & shopGames changes

--- gametran/src/components/Dashboard.js
+++ gametran/src/components/Dashboard.js
@@ -7,7 +7,7 @@ export default function Dashboard() {
   
         <ul>
           <li>
-            <Link  to="/">Home</Link>
+            <Link to="/">Home</Link>
           </li>
           <li>
             <Link to="/gameshop">GameShop</Link>
